87b471b41b817b9997e5d7e94c34c3999226e641
eugene-sew <eugenesew4@gmail.com>
2023-03-08 00:02:53 +0000

    modified for socode

--- cookie.js
+++ cookie.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", () => {
   const cookies = document.cookie.split("; ");
   const cookie = cookies.find((c) => c.startsWith("asst2="));
-  const deviceId = cookie ? cookie.split("=")[1] : null;
-  if (deviceId) {
+  // const deviceId = cookie ? cookie.split("=")[1] : null;
+  if (cookie) {
     vanquish();
     return;
   } else {
